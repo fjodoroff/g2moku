8d1f0fe31c0605ae2d53915bd7a3ea0c4f7aa62b
Maksim Maksimov <maksim@infobig.ee>
2015-10-13 01:19:40 +0300

    Added simple backend functionality. Showing game page. Added testing tools and code coverage possibility. Grunt tasks added: [server, test, coverage]

--- GruntFile.js
+++ GruntFile.js
@@ -19,30 +19,67 @@ module.exports = function(grunt) {
             }
           }
         },
+		jshint: {
+		  options: {
+			jshintrc: '.jshintrc',
+			ignores: ['test/coverage/**/*.js']
+		  },
+		  files: {
+			src: ['app/**/*.js', 'test/**/*.js']
+		  },
+		  gruntfile: {
+			src: 'Gruntfile.js'
+		  }
+		},
+		nodemon: {
+		  dev: {
+			script: 'app/main.js',
+			options: {
+			  ext: 'js,json'
+			}
+		  }
+		},
+		concurrent: {
+		  target: {
+			tasks: ['nodemon', 'watch'],
+			options: {
+			  logConcurrentOutput: true
+			}
+		  }
+		},
+		mochaTest: {
+		  unit: {
+			options: {
+			  reporter: 'spec'
+			},
+			src: ['test/unit/*.js']
+		  },
+		  route: {
+			options: {
+			  reporter: 'spec'
+			},
+			src: ['test/route/*.js']
+		  },
+		  // api: {
+			// options: {
+			  // reporter: 'spec'
+			// },
+			// src: ['test/api/*.js']
+		  // }
+		},
+		// start - code coverage settings
+		env: {
+		  coverage: {
+			APP_DIR_FOR_CODE_COVERAGE: '../test/coverage/instrument/app/'
+		  }
+		},
+			
 
-        jshint: {
-          files: ['gruntfile.js','<%= pkg.staddle.js %>/main.js','<%= pkg.staddle.js %>/modules/*.js','!<%= pkg.staddle.js %>/modules/flickr.js'],
-          options: {
-            globals: {
-              jQuery: true,
-              console: true
-            }
-          }
-        },
-
-        uglify: {
-          options: {
-              banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
-          },
-          my_target: {
-            files: {
-              '<%= pkg.staddle.site %>/<%= pkg.staddle.js %>/main.min.js': ['<%= pkg.staddle.js %>/main.js','<%= pkg.staddle.js %>/modules/*.js','!<%= pkg.staddle.js %>/modules/_template.js']
-            } 
-          }
-        },
-
-        clean: {
-          jslibs: [
+		clean: {
+		  coverage: {
+			src: ['test/coverage/']
+		  },
+		  jslibs: [
             '<%= pkg.staddle.js %>/libs/jquery/**/*',
             '!<%= pkg.staddle.js %>/libs/jquery/jquery.min.js',
             '<%= pkg.staddle.js %>/libs/jquery/.gitignore',
@@ -55,13 +92,87 @@ module.exports = function(grunt) {
             '!<%= pkg.staddle.js %>/libs/selectivizr/selectivizr.js'
           ],
           html: ['<%= pkg.staddle.site %>/**/*.html','!<%= pkg.staddle.site %>/<%= pkg.staddle.assets %>/**/*.html']
+		},
+
+
+		copy: {
+		  views: {
+			expand: true,
+			flatten: true,
+			src: ['app/views/*'],
+			dest: 'test/coverage/instrument/app/views'
+		  },
+		  assets: { // Not less, js or img
+            files: [ 
+              { 
+                expand: true,
+                src: ['<%= pkg.staddle.assets %>/**/*', '!<%= pkg.staddle.less %>/**/*', '!<%= pkg.staddle.js %>/**/*', '!<%= pkg.staddle.img %>/**/*'], 
+                dest: '<%= pkg.staddle.site %>/',
+                filter: 'isFile' 
+              }
+            ]
+          },
+          jslibs: { 
+            files: [
+              { 
+                src: '<%= pkg.staddle.js %>/libs/respond/respond.min.js', 
+                dest: '<%= pkg.staddle.site %>/<%= pkg.staddle.js %>/libs/respond/respond.min.js' 
+              },
+              { 
+                src: '<%= pkg.staddle.js %>/libs/selectivizr/selectivizr.js', 
+                dest: '<%= pkg.staddle.site %>/<%= pkg.staddle.js %>/libs/selectivizr/selectivizr.js' 
+              },
+              { 
+                src: '<%= pkg.staddle.js %>/libs/jquery/jquery.min.js', 
+                dest: '<%= pkg.staddle.site %>/<%= pkg.staddle.js %>/libs/jquery/jquery.min.js' 
+              } 
+            ]
+          }
+		},
+
+
+		instrument: {
+		  files: 'app/*.js',
+		  options: {
+			lazy: true,
+			basePath: 'test/coverage/instrument/'
+		  }
+		},
+
+
+		storeCoverage: {
+		  options: {
+			dir: 'test/coverage/reports'
+		  }
+		},
+
+
+		makeReport: {
+		  src: 'test/coverage/reports/**/*.json',
+		  options: {
+			type: 'lcov',
+			dir: 'test/coverage/reports',
+			print: 'detail'
+		  }
+		},
+
+		// end - code coverage settings
+        uglify: {
+          options: {
+              banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+          },
+          myTarget: {
+            files: {
+              '<%= pkg.staddle.site %>/<%= pkg.staddle.js %>/main.min.js': ['<%= pkg.staddle.js %>/main.js','<%= pkg.staddle.js %>/modules/*.js','!<%= pkg.staddle.js %>/modules/_template.js']
+            } 
+          }
         },
 
         imagemin: {
             options: {
                 optimizationLevel: 3
             },
-            dynamic_mappings: {
+            dynamicMappings: {
               files: [
                 {
                   expand: true, 
@@ -92,35 +203,6 @@ module.exports = function(grunt) {
               }
             ]
           }               
-        },
-
-        copy: {
-          assets: { // Not less, js or img
-            files: [ 
-              { 
-                expand: true,
-                src: ['<%= pkg.staddle.assets %>/**/*', '!<%= pkg.staddle.less %>/**/*', '!<%= pkg.staddle.js %>/**/*', '!<%= pkg.staddle.img %>/**/*'], 
-                dest: '<%= pkg.staddle.site %>/',
-                filter: 'isFile' 
-              }
-            ]
-          },
-          jslibs: { 
-            files: [
-              { 
-                src: '<%= pkg.staddle.js %>/libs/respond/respond.min.js', 
-                dest: '<%= pkg.staddle.site %>/<%= pkg.staddle.js %>/libs/respond/respond.min.js' 
-              },
-              { 
-                src: '<%= pkg.staddle.js %>/libs/selectivizr/selectivizr.js', 
-                dest: '<%= pkg.staddle.site %>/<%= pkg.staddle.js %>/libs/selectivizr/selectivizr.js' 
-              },
-              { 
-                src: '<%= pkg.staddle.js %>/libs/jquery/jquery.min.js', 
-                dest: '<%= pkg.staddle.site %>/<%= pkg.staddle.js %>/libs/jquery/jquery.min.js' 
-              } 
-            ]
-          }
         },
 
         connect: {
@@ -180,9 +262,18 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-istanbul');
+	grunt.loadNpmTasks('grunt-env');
 
     // Default Tasks
+	grunt.registerTask('server', ['concurrent:target']);
 	grunt.registerTask('work', ['less', 'jshint', 'connect', 'watch']);
-    grunt.registerTask('default', ['less','jshint','uglify','imagemin','clean','assemble','copy','connect','watch']);
+	grunt.registerTask('test', ['mochaTest:unit', 'mochaTest:route']);
+	grunt.registerTask('coverage', ['jshint', 'clean:coverage', 'copy:views', 'env:coverage',
+		'instrument', 'mochaTest:unit', 'mochaTest:route', 'storeCoverage', 'makeReport']);
+    grunt.registerTask('default', ['less','jshint','uglify','imagemin','assemble','copy','connect','watch']);
 
 };
